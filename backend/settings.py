27f48c90385bcdec6163e6f48e86b574c24beb79
Samuel <diegodsqs2@gmail.com>
2025-05-16 10:58:13 -0500

    pysql

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 from dotenv import load_dotenv
 import os
 
@@ -33,7 +34,7 @@ DEBUG = False
 ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOST", "*")]
 
 
-
+pymysql.install_as_MySQLdb()
 # Application definition
 
 INSTALLED_APPS = [
